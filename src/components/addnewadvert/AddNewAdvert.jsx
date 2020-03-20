6fc679fe1254f32c32806aba47561b4505ee6892
jkarelins <jkarelins@gmail.com>
2020-03-20 14:20:54 +0100

    new form for advert add

--- src/components/addnewadvert/AddNewAdvert.jsx
+++ src/components/addnewadvert/AddNewAdvert.jsx
@@ -1,10 +1,30 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { createAdvert } from "../../actions/advert";
+import AdvertForm from "./AdvertForm";
 
 const initialState = {
   description: "",
-  postcode: ""
+  isForSale: false,
+  isForRent: false,
+  price: 0,
+  address: "",
+  postcode: "",
+  sqrMeter: 0,
+  cubicMeter: 0,
+  nrOfRooms: 0,
+  nrOfBathrooms: 0,
+  nrOfFloors: 0,
+  locatedOnFloor: 0,
+  advertStatus: "published",
+  monthlyContribution: 0,
+  constructionYear: 2010,
+  renovationYear: 2010,
+  energyLabel: "",
+  heating: "",
+  warmWater: "",
+  storage: "",
+  parking: ""
 };
 
 class AddNewAdvert extends Component {
@@ -22,28 +42,34 @@ class AddNewAdvert extends Component {
     this.setState(initialState);
   };
 
+  forRentForSale = action => {
+    if (action === "sale") {
+      this.setState({
+        ...this.state,
+        isForSale: true,
+        isForRent: false
+      });
+    }
+    if (action === "rent") {
+      this.setState({
+        ...this.state,
+        isForSale: false,
+        isForRent: true
+      });
+    }
+  };
+
   render() {
     return (
       <div>
-        <form onSubmit={this.submitNewAdvert}>
-          <input
-            type="text"
-            name="description"
-            value={this.state.description}
-            onChange={this.handleChange}
-            placeholder="Description"
-            required
-          />
-          <input
-            type="text"
-            name="postcode"
-            value={this.state.postcode}
-            onChange={this.handleChange}
-            placeholder="Postcode"
-            required
-          />
-          <input type="submit" value="Add New" />
-        </form>
+        <hr />
+        <h4>Add New advertisement</h4>
+        <AdvertForm
+          submitNewAdvert={this.submitNewAdvert}
+          handleChange={this.handleChange}
+          formValues={this.state}
+          forRentForSale={this.forRentForSale}
+        />
       </div>
     );
   }
