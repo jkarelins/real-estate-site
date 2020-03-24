ab3daa1d5ba7804213ae62f230b02b7bb8c02619
jkarelins <jkarelins@gmail.com>
2020-03-24 15:57:24 +0100

    removed react errors

--- src/components/image/ImageGallery.jsx
+++ src/components/image/ImageGallery.jsx
@@ -6,7 +6,17 @@ export default class ImageGallery extends Component {
       <div>
         <h4>Gallery</h4>
         {this.props.advert.advert_images.map((imageCon, i) => (
-          <img key={i} src={imageCon.image.url} width="150" height="150" />
+          <img
+            key={i}
+            src={imageCon.image.url}
+            width="150"
+            height="150"
+            alt={`${this.props.advert.address}, property ${
+              this.props.advert.isForSale ? "for sale - " : "for rent - "
+            }${this.props.advert.price}${
+              this.props.advert.isForSale ? "EUR" : "EUR/month"
+            }`}
+          />
         ))}
       </div>
     );
