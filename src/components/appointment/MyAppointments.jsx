7792f5f3d5e0d50e5e05bbc5a06ca130f17425c7
jkarelins <jkarelins@gmail.com>
2020-03-26 11:07:10 +0100

    small changes to appointment

--- src/components/appointment/MyAppointments.jsx
+++ src/components/appointment/MyAppointments.jsx
@@ -14,6 +14,7 @@ class ShowAppointment extends Component {
   };
 
   render() {
+    console.log(this.props.appointments);
     if (this.props.appointments) {
       const { appointments, canceled } = this.props;
       return (
