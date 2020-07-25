9fbb0ab380ccb00e92afaf8e0052b6bd7a5fe0f2
J Karelins <jkarelins@gmail.com>
2020-07-25 20:11:30 +0200

    searchbar changes

--- src/components/searchbar/SearchBar.jsx
+++ src/components/searchbar/SearchBar.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./SearchBar.css"
 
 const initialState = {
   city: "",
@@ -75,97 +76,92 @@ export default class SearchBar extends Component {
   render() {
     // console.log(this.props);
     return (
-      <div className="container mt-3">
-        <div className="border border-secondary rounded">
-          <div className="m-3">
-            <form
-              // className="form-inline"
+      <div className="mt-3">
+        <div className="container">
+          <form
+              className="form-inline"
               onSubmit={e => this.searchByCityname(e)}
-            >
-              <div className="input-group my-3">
-                <label htmlFor="city" className="col-12">
-                  City Name
-                  <input
-                    className="form-control"
-                    type="search"
-                    placeholder="Input City Name"
-                    aria-label="Search"
-                    name="city"
-                    onChange={this.handleChange}
-                    value={this.state.city}
-                  />
-                </label>
-              </div>
-              <div className="input-group my-3">
-                <label htmlFor="priceFrom" className="col-6">
-                  Minimal Price
-                  <input
-                    className="form-control"
-                    type="number"
-                    placeholder="Price From"
-                    aria-label="Search"
-                    name="priceFrom"
-                    onChange={this.handleChange}
-                    value={this.state.priceFrom}
-                    min="1"
-                  />
-                </label>
+          >
+          <div className="form-row">
+            <div className="form-group col-12 col-md-3">
+              <label htmlFor="cityName" className="col-12">City Name</label>
+              <input
+                className="form-control col-12"
+                type="search"
+                placeholder="Input City Name"
+                aria-label="Search"
+                name="city"
+                onChange={this.handleChange}
+                value={this.state.city}
+              />
+            </div>
+            <div className="form-group col-6 col-md-2">
+              <label htmlFor="priceFrom" className="col-12">Price From</label>
+              <input
+                className="form-control col-12"
+                type="number"
+                placeholder="Price From"
+                aria-label="Search"
+                name="priceFrom"
+                onChange={this.handleChange}
+                value={this.state.priceFrom}
+                min="1"
+              />
+            </div>
+            <div className="form-group col-6 col-md-2">
+              <label htmlFor="priceTo" className="col-12">Price To</label>
+              <input
+                className="form-control col-12"
+                type="number"
+                placeholder="Price To"
+                aria-label="Search"
+                name="priceTo"
+                onChange={this.handleChange}
+                value={this.state.priceTo}
+                min="10"
+              />
+            </div>
+            <div className="d-flex align-items-end">
+              <button
+                type="button"
+                className={`btn ml-2 ${
+                  this.state.forRent ? "btn-success" : "btn-outline-warning"
+                }`}
+                onClick={() => this.forSaleOrRent("RENT")}
+              >
+                For Rent
+              </button>
+              <button
+                type="button"
+                className={`btn ml-2 ${
+                  this.state.forSale ? "btn-success" : "btn-outline-warning"
+                }`}
+                onClick={() => this.forSaleOrRent("SALE")}
+              >
+                For Sale
+              </button>
 
-                <label htmlFor="priceTo" className="col-6">
-                  Maximal Price
-                  <input
-                    className="form-control"
-                    type="search"
-                    placeholder="Price To"
-                    aria-label="Search"
-                    name="priceTo"
-                    onChange={this.handleChange}
-                    value={this.state.priceTo}
-                    min="0"
-                  />
-                </label>
-                <div className="input-group my-3 ml-5">
-                  <button
-                    type="button"
-                    className={`btn btn-sm ${
-                      this.state.forRent ? "btn-success" : "btn-outline-warning"
-                    }`}
-                    onClick={() => this.forSaleOrRent("RENT")}
-                  >
-                    For Rent
-                  </button>
-                  <button
-                    type="button"
-                    className={`btn btn-sm ml-2 ${
-                      this.state.forSale ? "btn-success" : "btn-outline-warning"
-                    }`}
-                    onClick={() => this.forSaleOrRent("SALE")}
-                  >
-                    For Sale
-                  </button>
-
-                  <input
-                    type="submit"
-                    value="Search"
-                    className="btn btn-outline-success ml-auto"
-                  />
-                </div>
-              </div>
-            </form>
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-outline-success ml-2"
+              />
+            </div>
           </div>
-
-          <div className="col text-center mb-2">
-            <small className="text-danger">
-              {this.state.noSearchTerm ? "Please define some search term" : ""}
-              {this.state.minMoreThenMax
-                ? "Minimal price should be less then maximal"
-                : ""}
-              {this.state.noRentNoSale
-                ? "For Rent or For Sale should be selected"
-                : ""}
-            </small>
-          </div>
+        </form>
         </div>
+        <div className="col text-center mb-2">
+          <small className="text-danger">
+            {this.state.noSearchTerm ? "Please define some search term" : ""}
+            {this.state.minMoreThenMax
+              ? "Minimal price should be less then maximal"
+              : ""}
+            {this.state.noRentNoSale
+              ? "For Rent or For Sale should be selected"
+              : ""}
+          </small>
+        </div>
+        <hr className="hrSection" />
       </div>
     );
   }
