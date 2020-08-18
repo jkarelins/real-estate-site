ef77dfffa0e7520b20ba09778a4a24c2d6a31a8e
J Karelins <jkarelins@gmail.com>
2020-08-18 21:14:42 +0200

    ch

--- src/components/mainpage/MainPage.jsx
+++ src/components/mainpage/MainPage.jsx
@@ -54,24 +54,26 @@ class MainPage extends Component {
       return (
         <Fragment>
           <SearchBar history={this.props.history} />
-          <div className="row mt-3 d-flex justify-content-center">
-            {this.props.allAdverts.map((advert, i) => (
-              <AdvertCard advert={advert} key={i} />
-            ))}
-          </div>
+          <div className="container">
+            <div className="row mt-3 d-flex justify-content-center">
+              {this.props.allAdverts.map((advert, i) => (
+                <AdvertCard advert={advert} key={i} />
+              ))}
+            </div>
 
-          <div className="d-flex justify-content-center mt-3 mb-5">
-            {this.props.advertsCount <= this.state.offset ? (
-              <p className="text-danger">
-                {this.state.noMoreAdverts
-                  ? "Sorry, no more advertisements"
-                  : "Loading..."}
-              </p>
-            ) : (
-              <button className="btn btn-primary" onClick={this.loadMore}>
-                Load More
-              </button>
-            )}
+            <div className="d-flex justify-content-center mt-3 mb-5">
+              {this.props.advertsCount <= this.state.offset ? (
+                <p className="text-danger">
+                  {this.state.noMoreAdverts
+                    ? "Sorry, no more advertisements"
+                    : "Loading..."}
+                </p>
+              ) : (
+                <button className="btn btn-primary" onClick={this.loadMore}>
+                  Load More
+                </button>
+              )}
+            </div>
           </div>
         </Fragment>
       );
